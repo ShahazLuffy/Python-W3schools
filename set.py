6b2c6b7520cc2745e67b41ea53d5bc92f51b1b08
Shahaz <ali.ghaderipour@gmail.com>
2023-08-25 21:47:12 +0330

    test commit

--- set.py
+++ set.py
@@ -1,10 +1,13 @@
 # set
-my_set = {1,2,2,2,2,4,5}
+my_set = {1, 2, 2, 2, 2, 4, 5}
 my_set.add(2)
 my_set.add(552)
 print(my_set)
 
-duplicated_list = [58,58,24,24,24,24,24,24 ,24, 48, 69, 28, 64, 37,18, 26, 85, 69, 59, 51, 25, 67, 18]
-print(set(duplicated_list)) # set has no duplicate value
+duplicated_list = [58, 58, 24, 24, 24, 24, 24, 24, 24, 48,
+                   69, 28, 64, 37, 18, 26, 85, 69, 59, 51, 25, 67, 18]
+print(set(duplicated_list))  # set has no duplicate value
 no_duplicated_set = set(duplicated_list)
 print(67 in no_duplicated_set)
+
+# asfasdfasdfasdfasdf
